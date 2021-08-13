d54d0abbd7b08da2a588e8239ab46f04a2d26c15
Marcelo Bomfim <celodc@yahoo.com.br>
2021-08-13 17:08:16 -0300

    controle de ponto parte 2

--- bootcampSantander2021/controlePonto/Readme.md
+++ bootcampSantander2021/controlePonto/Readme.md
@@ -11,3 +11,15 @@ Framework: SpringBoot, Hibernate, Lombok, swagger
 Gerenciador de dependências: Gladde
 
 Banco de Dados: H2
+
+1 parte: 
+
+* criação do projeto com springBoot io
+* Configuração do application.properties
+* Configuração do build.gradle
+* Configuração do OpenAPI(swagger-ui)
+* criação das classes modelos.
+
+2 parte:
+
+* execução e configuração do banco de dados h2 na memoria
--- bootcampSantander2021/controlePonto/src/main/java/com/bootCampSantander/controlePonto/ControlePontoApplication.java
+++ bootcampSantander2021/controlePonto/src/main/java/com/bootCampSantander/controlePonto/ControlePontoApplication.java
@@ -1,13 +1,32 @@
 package com.bootCampSantander.controlePonto;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ControlePontoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlePontoApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI().info(
+								new Info()
+									.title("Sistema de Controle de Ponto")
+									.version("1.0")
+									.termsOfService("http://swagger.io/terms")
+									.license(
+											new License()
+													.name("Apache 2.0")
+													.url("http://springdoc.org")
+											)
+								);
 	}
 
 }
